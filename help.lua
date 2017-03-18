1ea731319cf5e2c25fb3b0be5ba3272da8db3c3b
chenxiaodong <chenxiaodong@xiaomi.com>
2017-03-18 17:22:13 +0800

    feat: add help

--- help.lua
+++ help.lua
@@ -1,119 +1,48 @@
 -- https://github.com/ashfinal/awesome-hammerspoon/blob/master/modes/cheatsheet.lua
 function generateHtml()
-    local appTitle = 'CheatSheets 开发中...'
-    local myMenuItems = '<div style="margin-top:30px;text-align:center;">prefix + ? 关闭当前窗口</dev>'
+  return [[<pre>  Cheatsheets
 
-    local html = [[
-        <!DOCTYPE html>
-        <html>
-        <head>
-        <style type="text/css">
-            *{margin:0; padding:0;}
-            html, body{
-              background-color:#eee;
-              font-family: arial;
-              font-size: 13px;
-            }
-            a{
-              text-decoration:none;
-              color:#000;
-              font-size:12px;
-            }
-            li.title{ text-align:center;}
-            ul, li{list-style: inside none; padding: 0 0 5px;}
-            footer{
-              position: fixed;
-              left: 0;
-              right: 0;
-              height: 48px;
-              background-color:#eee;
-            }
-            header{
-              position: fixed;
-              top: 0;
-              left: 0;
-              right: 0;
-              height:48px;
-              background-color:#eee;
-              z-index:99;
-            }
-            footer{ bottom: 0; }
-            header hr,
-            footer hr {
-              border: 0;
-              height: 0;
-              border-top: 1px solid rgba(0, 0, 0, 0.1);
-              border-bottom: 1px solid rgba(255, 255, 255, 0.3);
-            }
-            .title{
-                padding: 15px;
-            }
-            li.title{padding: 0  10px 15px}
-            .content{
-              padding: 0 0 15px;
-              font-size:12px;
-              overflow:hidden;
-            }
-            .content.maincontent{
-            position: relative;
-              height: 577px;
-              margin-top: 46px;
-            }
-            .content > .col{
-              width: 23%;
-              padding:20px 0 20px 20px;
-            }
-            li:after{
-              visibility: hidden;
-              display: block;
-              font-size: 0;
-              content: " ";
-              clear: both;
-              height: 0;
-            }
-            .cmdModifiers{
-              width: 60px;
-              padding-right: 15px;
-              text-align: right;
-              float: left;
-              font-weight: bold;
-            }
-            .cmdtext{
-              float: left;
-              overflow: hidden;
-              width: 165px;
-            }
-        </style>
-        </head>
-          <body>
-            <header>
-              <div class="title"><strong>]]..appTitle..[[</strong></div>
-              <hr />
-            </header>
-            <div class="content maincontent">]]..myMenuItems..[[</div>
-            <br>
-          <footer>
-            <hr />
-              <div class="content" >
-                <div class="col">
-                  by <a href="https://github.com/cxdongjack" target="_parent">chenxiaodong</a>
-                </div>
-              </div>
-          </footer>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/2.2.2/isotope.pkgd.min.js"></script>
-            <script type="text/javascript">
-              var elem = document.querySelector('.content');
-              var iso = new Isotope( elem, {
-                // options
-                itemSelector: '.col',
-                layoutMode: 'masonry'
-              });
-            </script>
-          </body>
-        </html>
-        ]]
+  !!!prefix + ? to exist
 
-    return html
+  Tmux style hotkeys
+  This configuration heavily uses Tmux style hotkey binding, i.e., using a prefix to activate hotkeys.
+  The default prefix is `ctrl+v`, e.g., `prefix - w` = press and release `ctrl+v` first, then press `w`.
+
+  - `prefix - ?`: help.
+  - `prefix - g`: show a 3x3 grid for resizing window.
+  - `prefix - w`: switcher for windows(while system switcher for application).
+  - `prefix - c`: toggle caffeinate (disable sleep).
+  - `prefix - s`: sleep.
+  - `prefix - n`: toggle night shift.
+  - `prefix - r`: reload Hammerspoon config.
+  - `prefix - d`: toggle Hammerspoon console for debug.
+  - `prefix - m`: enter mouse mode, then you can use `hjkl` to move the mouse, use `cmd+hjkl` to move slowly, use `shift+hjkl` to scroll, use `u`, `i`, `o` to perform left, right, middle button click.
+
+# Normal Keymaps
+  The option key is used for inputing special characters, may mapping option key be a better offer.
+
+  - `option [ + shift ] + tab`: switch windows.
+  - `option + npfb`: arrow keys.
+  - `option + ctrl + npfb`: option + arrow keys (move faster).
+  - `option + a`: home.
+  - `option + e`: end.
+  - `option + u`: page_up.
+  - `option + d`: page_down.
+  - `option + [/]`: cmd [+ shift] + tab (switch tabs).
+  - `option + q`: esc.
+  - `option + m` / `option + 1`: play.
+  - `option + i` / `option + 2`: previous track.
+  - `option + o` / `option + 3`: next track.
+  - `option + /` / `option + f1`: mute.
+  - `option + ,` / `option + f2`: volume down.
+  - `option + .` / `option + f3`: volume up.
+  - `press ctrl twice`: escape
+  - `press cmd Q twice`: quit app, in order to prevent accidentally quitting an app when pressing `cmd+q`.
+
+#  Desktop Client enhancements
+  - Switching to IM(Wechat, QQ) window automatically opens IM's "find" dialog
+</pre>
+]]
 end
 
 
@@ -131,7 +60,7 @@ function showCheatsheet()
         :windowStyle("utility")
         :closeOnEscape(true)
         :html(generateHtml())
-        -- :url('http://baidu.com')
+        -- :url('http://github.com/cxdongjack/hammerspoon/blob/master/README.md')
         :allowGestures(true)
         :allowNewWindows(false)
         :windowTitle("CheatSheets")
