53134dd82ebb26ab0ed80b83ab819691f56346b6
chenxiaodong <chenxiaodong@xiaomi.com>
2017-03-23 08:31:23 +0800

    opt: fullscreen undo

--- window.lua
+++ window.lua
@@ -15,6 +15,29 @@ prefix.bind('', 'g', function() hs.grid.show() end)
 ----------------
 prefix.bind('', 'z', function()
     local win = hs.window.focusedWindow()
-    win:move({0, 0, 1, 1})
+    if win and not undo[win:id()] then
+        undo:push()
+        -- fullscreen
+        win:move({0, 0, 1, 1})
+    else
+        undo:pop()
+    end
 end)
 
+-- undo for window operations
+undo = {}
+
+function undo:push()
+    local win = hs.window.focusedWindow()
+  if win and not undo[win:id()] then
+    self[win:id()] = win:frame()
+  end
+end
+
+function undo:pop()
+    local win = hs.window.focusedWindow()
+  if win and self[win:id()] then
+    win:setFrame(self[win:id()])
+    self[win:id()] = nil
+  end
+end
